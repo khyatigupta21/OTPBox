790d4809ccfb123e1832b8b7da9cc88c72ae6162
Khyati Gupta <khyatigupta21@gmail.com>
2021-11-25 13:33:30 +0800

    initial commit

---
+++ src/RefEx.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+
+const refSearch = () => {
+  const [search, setSearch] = useState("");
+    const _searchRef = useRef<any>({});
+  
+  const _handleSubmit = () => {
+    console.log({ "Search result is": _searchRef.current.value });
+  };
+  useEffect(() => {
+    _searchRef.current.focus();
+  });
+  return (
+    <div className="App">
+      <h1>Search using UseRef</h1>
+      {/* <input
+        type="text"
+        placeholder="Search your user"
+        onChange={(e: any) => setSearch(e.target.value)}
+      /> */}
+      <input type="text" placeholder="Search your user" ref={_searchRef} />
+      <button className="useRefBtn" onClick={_handleSubmit}>
+        Save Details
+      </button>
+    </div>
+  );
+};
+
+export default refSearch ;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import OtpSearch from './OtpBox'
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <OtpSearch/>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
